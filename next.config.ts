2d48d2f1a8c90956478ad317394863e14ca4ff27
Ny Aro <nyarodina@gmail.com>
2026-07-17 06:41:20 +0300

    chore: remove unused picsum.photos domains from image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -16,14 +16,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "picsum.photos",
-      },
-      {
-        protocol: "https",
-        hostname: "fastly.picsum.photos",
-      },
-      {
-        protocol: "https",
         hostname: "zquwcnrplxtumsauwbea.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
